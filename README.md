3228fe043f5e2992872bba22333b1fbe72b1815a
Rathinavel <rathinavelsubramani56@gmail.com>
2020-03-08 02:08:45 +0530

    Initial

--- README.md
+++ README.md
@@ -1,22 +1,10 @@
-# SB Admin rewritten in Angular9 and Bootstrap 4
-
-Simple Dashboard Admin App built using Angular 9 and Bootstrap 4
-
-This project is a port of the famous Free Admin Bootstrap Theme [SB Admin v8.0](http://startbootstrap.com/template-overviews/sb-admin-2/) to Angular9 Theme.
-
-Powered by [StartAngular](http://startangular.com/) & [StrapUI](http://strapui.com/)
-
-## [Demo](http://rawgit.com/start-angular/SB-Admin-BS4-Angular-6/master/dist/)
-
-## [SB Admin Material version](https://github.com/start-javascript/sb-admin-material)
-
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.0.2.
 
 ### Introduction
 
 Provides fast, reliable and extensible starter for the development of Angular projects.
 
-`sb-admin-bs4-angular9` provides the following features:
+`IMA` provides the following features:
 
 *   Developed using boostrap-v6.0.0
 *   angular-v9.0.2
@@ -36,8 +24,6 @@ Provides fast, reliable and extensible starter for the development of Angular pr
 In order to start the project use:
 
 ```bash
-$ git clone https://github.com/start-angular/SB-Admin-BS4-Angular-8.git
-$ cd SB-Admin-BS4-Angular-8
 # install the project's dependencies
 $ npm install
 # watches your files and uses livereload by default run `npm start` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
--- src/app/layout/charts/charts.component.ts
+++ src/app/layout/charts/charts.component.ts
@@ -30,31 +30,6 @@ export class ChartsComponent implements OnInit {
         { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
     ];
 
-    // Doughnut
-    public doughnutChartLabels: string[] = [
-        'Download Sales',
-        'In-Store Sales',
-        'Mail-Order Sales'
-    ];
-    public doughnutChartData: number[] = [350, 450, 100];
-    public doughnutChartType: string;
-
-    // Radar
-    public radarChartLabels: string[] = [
-        'Eating',
-        'Drinking',
-        'Sleeping',
-        'Designing',
-        'Coding',
-        'Cycling',
-        'Running'
-    ];
-    public radarChartData: any = [
-        { data: [65, 59, 90, 81, 56, 55, 40], label: 'Series A' },
-        { data: [28, 48, 40, 19, 96, 27, 100], label: 'Series B' }
-    ];
-    public radarChartType: string;
-
     // Pie
     public pieChartLabels: string[] = [
         'Download Sales',
@@ -63,19 +38,6 @@ export class ChartsComponent implements OnInit {
     ];
     public pieChartData: number[] = [300, 500, 100];
     public pieChartType: string;
-
-    // PolarArea
-    public polarAreaChartLabels: string[] = [
-        'Download Sales',
-        'In-Store Sales',
-        'Mail Sales',
-        'Telesales',
-        'Corporate Sales'
-    ];
-    public polarAreaChartData: number[] = [300, 500, 100, 40, 120];
-    public polarAreaLegend: boolean;
-
-    public polarAreaChartType: string;
 
     // lineChart
     public lineChartData: Array<any> = [
@@ -163,11 +125,7 @@ export class ChartsComponent implements OnInit {
     ngOnInit() {
         this.barChartType = 'bar';
         this.barChartLegend = true;
-        this.doughnutChartType = 'doughnut';
-        this.radarChartType = 'radar';
         this.pieChartType = 'pie';
-        this.polarAreaLegend = true;
-        this.polarAreaChartType = 'polarArea';
         this.lineChartLegend = true;
         this.lineChartType = 'line';
     }
