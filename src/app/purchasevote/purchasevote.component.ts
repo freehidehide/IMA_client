6bc9e38c074e95efa3c7b9fe3f9604fd91868ee2
Steve Austin <spannan78@gmail.com>
2020-03-12 01:14:09 +0530

    added purchase vote.

---
+++ src/app/purchasevote/purchasevote.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-purchasevote',
+  templateUrl: './purchasevote.component.html',
+  styleUrls: ['./purchasevote.component.scss']
+})
+export class PurchasevoteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/purchasevote/purchasevote.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PurchasevoteRoutingModule } from './purchasevote-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PurchasevoteRoutingModule
+  ]
+})
+export class PurchasevoteModule { }
