bc660b7c804ba9b8e4837088cb07fa1c343ddc4f
freehidehide <freehidehide@gmail.com>
2020-03-29 18:10:50 +0530

    edit profile api implementation

--- src/app/editprofile/editprofile.component.ts
+++ src/app/editprofile/editprofile.component.ts
@@ -2,7 +2,7 @@
 
 import {Component, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ToastService } from '../api/services/toast-service';
 import { UserService } from '../api/services/user.service';
 import { SessionService } from '../api/services/session-service';
@@ -14,10 +14,11 @@ import { UserBaseComponent } from '../user.base.component';
 	styleUrls: ['./editprofile.component.scss']
 })
 export class EditprofileComponent extends UserBaseComponent implements OnInit {
-
+    public isSubmitted: boolean;
     public userId: number;
     public userClass = 'UserAvatar';
     public multiple = 'multiple';
+    public editProfileForm: FormGroup;
 	constructor(
         protected router: Router,
 		private formBuilder: FormBuilder,
@@ -29,8 +30,46 @@ export class EditprofileComponent extends UserBaseComponent implements OnInit {
     }
 
     ngOnInit(): void {
+        this.editProfileForm = this.formBuilder.group({
+            first_name: ['', [Validators.required]],
+            last_name: ['', [Validators.required]],
+            // email: ['', [Validators.required, Validators.email]],
+            username: ['', [Validators.required]],
+            address: this.formBuilder.group({
+                addressline1: ['', [Validators.required]],
+                addressline2: ['', [Validators.required]],
+                city: ['', [Validators.required]],
+                state: ['', [Validators.required]],
+                country: ['', [Validators.required]],
+                zipcode: ['', [Validators.required]]
+            })
+        });
         this.userId = this.sessionService.user.id;
         this.getUser();
+    }
+
+    get f() {
+		return this.editProfileForm.controls;
+	}
+
+    onSubmit() {
+        this.isSubmitted = true;
+        if (this.editProfileForm.invalid) {
+			return;
+        }
+        this.toastService.showLoading();
+		this.userService
+			.updateUser(this.editProfileForm.value)
+			.subscribe((data) => {
+				this.isSubmitted = false;
+				this.toastService.clearLoading();
+				if (data.error.code) {
+					this.toastService.error(data.error.message);
+				} else {
+					this.toastService.success(data.error.message);
+				}
+			});
+
     }
 
 
