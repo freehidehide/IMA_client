924ad5bc770a09481289ce335afcf2c7787c1e0d
freehidehide <rathinavelsubramani56@gmail.com>
2020-04-23 22:02:46 +0530

    Changes

--- src/app/shared-common/attachment/attachment.component.ts
+++ src/app/shared-common/attachment/attachment.component.ts
@@ -2,6 +2,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Md5 } from 'ts-md5/dist/md5';
 import { Attachment } from 'src/app/api/models/attachment';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 // import { environment } from "../../environments/environment";
 
 @Component({
@@ -11,6 +12,7 @@ import { Attachment } from 'src/app/api/models/attachment';
 })
 export class AttachmentComponent {
     public url: string;
+    public videoUrl: string;
     public imageClass = 'original';
     public cssClassString: string;
     public isVideo: boolean;
@@ -30,7 +32,7 @@ export class AttachmentComponent {
     @Input('attachment')
     set attachment(value: Attachment) {
         this.imageClass = 'original';
-        this.isVideo = (value && value.thumb) ? true : false;
+        this.isVideo = (value && value.thumb);
         if (!this.isVideo && value && value.id) {
             const id: string = value.id.toString();
             const filename: string = value.filename.split('.').pop();
@@ -47,9 +49,14 @@ export class AttachmentComponent {
                 '.' + filename;
         } else if (this.isVideo && value.thumb.id) {
             const id: string = value.thumb.id.toString();
+            const idVideo: string = value.id.toString();
             const filename: string = value.thumb.filename.split('.').pop();
+            const fileVideoName: string = value.filename.split('.').pop();
             const hash: string = Md5.hashStr(
                 value.thumb.class + id + filename + this.imageClass
+            ).toString();
+            const hashVideo: string = Md5.hashStr(
+                value.class + idVideo + fileVideoName + this.imageClass
             ).toString();
             this.url =
                 'http://app.itstheimas.com/images/' + this.imageClass + '/' +
@@ -59,6 +66,13 @@ export class AttachmentComponent {
                 '.' +
                 hash +
                 '.' + filename;
+            this.videoUrl = 'http://app.itstheimas.com/images/' + this.imageClass + '/' +
+                            value.class +
+                            '/' +
+                            idVideo +
+                            '.' +
+                            hashVideo +
+                            '.' + fileVideoName;
         } else {
             this.url =
                 'https://tanzolymp.com/images/default-non-user-no-photo-1.jpg';
@@ -68,6 +82,7 @@ export class AttachmentComponent {
     open(content) {
         this.modalReference = this.modalService.open(content);
         this.modalReference.result.then((result) => {
+            this.isPlayVideo = true;
         }, (reason) => {
         });
     }
