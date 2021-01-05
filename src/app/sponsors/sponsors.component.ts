25a9f9c54474e503358ad66e74ad90673273de0b
veeres1590 <veereswaran@cardinality.ai>
2021-01-05 21:07:01 +0530

    Changes

--- src/app/sponsors/sponsors.component.ts
+++ src/app/sponsors/sponsors.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-sponsors',
@@ -10,6 +11,44 @@ export class SponsorsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    $(document).ready(function () {
+      $(".clsclientBlock").slick({
+        dots: false,
+        infinite: true,
+        speed: 1e3,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3e3,
+        arrows: false,
+        responsive: [
+          {
+            breakpoint: 991,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 2,
+              arrows: !1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              arrows: !1
+            }
+          },
+          {
+            breakpoint: 400,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              arrows: !1
+            }
+          }
+        ]
+      });
+    });
   }
 
 }
